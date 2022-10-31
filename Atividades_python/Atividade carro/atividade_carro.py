34f445d14b2988fdc8430628b4927b9fc7b45038
Navarone <brunohenrique.travessa@gmail.com>
2022-10-31 18:26:21 -0300

    upando o arquivo da atividade dos carros -python-

---
+++ Atividades_python/Atividade carro/atividade_carro.py
@@ -0,0 +1,18 @@
+qtd_rodas = 2
+qtd_pessoas = 0
+peso_carro = 0
+
+if qtd_rodas <= 3:
+    print("Veiculo muito pequeno")
+
+elif qtd_rodas == 4 and qtd_pessoas <= 8 and peso_carro <= 3500:
+    print("Este veículo possui 4 rodas, suporta até 8 pessoas e seu peso máximo é de até 3500 kg")
+
+elif qtd_rodas >= 4 and peso_carro >= 500 and peso_carro <= 6000:
+    print("Este carro possui 4 rodas ou mais e seu peso está entre 500 kg e 6000 kg")
+
+elif qtd_rodas >= 4 and qtd_pessoas > 8:
+    print("Este carro possui 4 rodas ou mais e consegue carregar mais de 8 pessoas")
+
+elif qtd_rodas >= 4 and peso_carro > 6000:
+    print("Este veículo possui 4 rodas ou mais e seu peso é maior que 6000 kg")
