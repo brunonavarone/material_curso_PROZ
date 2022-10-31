1459cfb0b6da7069ef278f7164eec3c914b51d65
Navarone <brunohenrique.travessa@gmail.com>
2022-10-31 18:35:33 -0300

    editando o código dos carros

--- Atividades_python/Atividade carro/atividade_carro.py
+++ Atividades_python/Atividade carro/atividade_carro.py
@@ -3,16 +3,16 @@ qtd_pessoas = 0
 peso_carro = 0
 
 if qtd_rodas <= 3:
-    print("Veiculo muito pequeno")
+    print("Veículo da categoria A")
 
 elif qtd_rodas == 4 and qtd_pessoas <= 8 and peso_carro <= 3500:
-    print("Este veículo possui 4 rodas, suporta até 8 pessoas e seu peso máximo é de até 3500 kg")
+    print("Este veículo possui 4 rodas, suporta até 8 pessoas e seu peso máximo é de até 3500 kg - Categoria B")
 
 elif qtd_rodas >= 4 and peso_carro >= 500 and peso_carro <= 6000:
-    print("Este carro possui 4 rodas ou mais e seu peso está entre 500 kg e 6000 kg")
+    print("Este carro possui 4 rodas ou mais e seu peso está entre 500 kg e 6000 kg - Categoria C")
 
 elif qtd_rodas >= 4 and qtd_pessoas > 8:
-    print("Este carro possui 4 rodas ou mais e consegue carregar mais de 8 pessoas")
+    print("Este carro possui 4 rodas ou mais e consegue carregar mais de 8 pessoas - Categoria D")
 
 elif qtd_rodas >= 4 and peso_carro > 6000:
-    print("Este veículo possui 4 rodas ou mais e seu peso é maior que 6000 kg")
+    print("Este veículo possui 4 rodas ou mais e seu peso é maior que 6000 kg - Categoria E")
